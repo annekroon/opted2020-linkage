2f1a14f8aa902d5965e3686179b8b5f2c7f49bfa
annekroon <annekroon@gmail.com>
2021-12-20 16:16:52 +0100

    update

--- src/traditional_sml.py
+++ src/traditional_sml.py
@@ -3,12 +3,9 @@ from tqdm import tqdm
 from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from string import punctuation
-
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
-
-
 import csv
 from collections import Counter
 import matplotlib.pyplot as plt
@@ -20,6 +17,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
@@ -27,78 +25,115 @@ from sklearn.metrics import f1_score, make_scorer, classification_report
 import pandas as pd
 
 
-def down_sample_majority(df, majortopic):
-    df_majority = df[df[majortopic]==0]
-    df_minority = df[df[majortopic]==1]
-    df_majority_downsampled = resample(df_majority,
-                                     replace=False,     #
-                                     n_samples=len(df[df[majortopic]==1]), # set to N of minority topic
-                                     random_state=123) #
+class Classifiers():
 
-    df_downsampled = pd.concat([df_minority, df_majority_downsampled])
-    return df_downsampled
+    """In-sample and out-sample topic prediction based on CAP and CMP coding scheme.
 
-def train_classifiers(dataset, majortopic):
+    This class applies a set of traditional SML classifiers on different samples (more specifically, CAP and CMP data)
+    to get some in-sample and out-sample predictions.
+    """
 
-    print(f"majortopic:{majortopic}")
+    def __init__(self, trainingdata, evaluationdata):
 
-    df = down_sample_majority(dataset, majortopic)
+        self.datasource_1 = trainingdata # datasource_1 = pandas df, e.g., CAP data
+        self.datasource_2 = evaluationdata  # datasource_2 = pandas df, e.g., CMP data
+        self.names = ["Naive Bayes", "Linear SVM", "Logistic Regression", "Random Forest" ]
+        self.classifiers = [MultinomialNB(), LinearSVC(), LogisticRegression(), RandomForestClassifier() ]
+        self.parameters = [
+                      {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'clf__alpha': (1e-2, 1e-3)},
+                      {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'clf__C': (np.logspace(-5, 1, 5))},
+                      {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'clf__C': (np.logspace(-5, 1, 5))},
+                      {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'clf__max_depth': (1, 2)}
+                     ]
 
-    X_train, X_test, y_train, y_test = train_test_split(df['text'], df[majortopic], test_size=0.2, random_state=42)
+    def down_sample_majority(self, df, majortopic):
 
-    names = [
-             "Naive Bayes",
-             "Linear SVM",
-             "Logistic Regression",
-             "Random Forest"
-            ]
+        df_majority = df[df[majortopic]==0]
+        df_minority = df[df[majortopic]==1]
+        df_majority_downsampled = resample(df_majority,
+                                         replace=False,     #
+                                         n_samples=len(df[df[majortopic]==1]), # set to N of minority topic
+                                         random_state=123) #
 
-    classifiers = [
-        MultinomialNB(),
-        LinearSVC(),
-        LogisticRegression(),
-        RandomForestClassifier()
-    ]
+        df_downsampled = pd.concat([df_minority, df_majority_downsampled])
 
-    parameters = [
-                  {'vect__ngram_range': [(1, 1), (1, 2)],
-                  'clf__alpha': (1e-2, 1e-3)},
-                  {'vect__ngram_range': [(1, 1), (1, 2)],
-                  'clf__C': (np.logspace(-5, 1, 5))},
-                  {'vect__ngram_range': [(1, 1), (1, 2)],
-                  'clf__C': (np.logspace(-5, 1, 5))},
-                  {'vect__ngram_range': [(1, 1), (1, 2)],
-                  'clf__max_depth': (1, 2)}
-                 ]
+        return df_downsampled
 
 
-    class_report = []
+    def train_classifiers(self, majortopic):
 
-    f1 = make_scorer(f1_score, average='macro') # macro --> emphasizes the prediction of the smaller classes (in this case: hateful/ spam)
+        print(f"majortopic:{majortopic}")
 
-    for vec, n in tqdm(zip([CountVectorizer(stop_words='english'), TfidfVectorizer(stop_words='english')], ["Count", "Tfidf"])):
-        for name, classifier, params in zip(names, classifiers, parameters):
-            clf_pipe = Pipeline([
-                ('vect', vec),
-                ('clf', classifier),
-            ])
+        df = self.down_sample_majority(self.datasource_1, majortopic)
 
-            gs_clf = GridSearchCV(clf_pipe, param_grid=params, cv=3, n_jobs=-1, scoring=f1) #or; scoring='accuracy' if you don't want to create the f1 object yourself (see above)
-            clf = gs_clf.fit(X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(df['text'], df[majortopic], test_size=0.2, random_state=42)
 
-            score = clf.score(X_test, y_test)
-            print("{} score: {}".format(name, score))
 
-            print("{} are the best estimators".format(clf.best_estimator_))
 
-            results_to_dict = classification_report((clf.best_estimator_.predict(X_test)), y_test, output_dict= True)
+        class_report_insample = []
+        class_report_outsample = []
 
-            results_to_dict['classifier'] = name
-            results_to_dict['parameters'] = clf.best_params_
-            results_to_dict['vectorizer'] = n
-            class_report.append(results_to_dict)
-    return class_report
+        f1 = make_scorer(f1_score, average='macro') # macro --> emphasizes the prediction of the smaller classes (in this case: hateful/ spam)
 
+
+        for vec, n in tqdm(zip([CountVectorizer(stop_words='english'), TfidfVectorizer(stop_words='english')], ["Count", "Tfidf"])):
+            for name, classifier, params in zip(self.names, self.classifiers, self.parameters):
+                clf_pipe = Pipeline([
+                    ('vect', vec),
+                    ('clf', classifier),
+                ])
+
+                gs_clf = GridSearchCV(clf_pipe, param_grid=params, cv=3, n_jobs=-1, scoring=f1) #or; scoring='accuracy' if you don't want to create the f1 object yourself (see above)
+                clf = gs_clf.fit(X_train, y_train)
+
+                score = clf.score(X_test, y_test)
+
+                print("{} score: {}".format(name, score))
+                print("{} are the best estimators".format(clf.best_estimator_))
+
+                results_to_dict = classification_report((clf.best_estimator_.predict(X_test)), y_test, output_dict= True)
+                results_to_dict['classifier'] = name
+                results_to_dict['parameters'] = clf.best_params_
+                results_to_dict['vectorizer'] = n
+                class_report_insample.append(results_to_dict)
+
+                results_to_dict = classification_report((clf.best_estimator_.predict(self.datasource_2['text'])), self.datasource_2[majortopic], output_dict= True)
+                results_to_dict['classifier'] = name
+                results_to_dict['parameters'] = clf.best_params_
+                results_to_dict['vectorizer'] = n
+                class_report_outsample.append(results_to_dict)
+
+        return class_report_insample, class_report_outsample
+
+    def clean_classification_report(self, class_report):
+
+        appended_data = []
+        for i in class_report:
+            data = pd.DataFrame.from_dict(i)
+            appended_data.append(data)
+
+        df = pd.concat(appended_data)
+        df['indicators'] = df.index
+
+        return df
+
+    def get_classification_results(self, major_topic):
+        """This function will train several classifiers and return performance metrics using the original data"""
+
+        appended_data = []
+
+        print(f"starting classification of the topic ... {major_topic}....")
+
+        class_report_insample, class_report_outsample = self.train_classifiers(major_topic)
+
+        df_insample = self.clean_classification_report(class_report_insample)
+        df_outsample = self.clean_classification_report(class_report_outsample)
+
+        return df_insample, df_outsample
 
 def clean_metrics(df):
     i = ['precision', 'recall', 'support', 'f1-score']
